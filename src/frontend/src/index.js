aa76c7cb812b348b50249e240a841856d8e53ab0
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-05-19 17:14:40 +0300

    remove amplify deps for now causing problems

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -3,12 +3,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Amplify } from 'aws-amplify'
 import App from './App'
-
-import awsExports from './aws-exports'
-
-Amplify.configure(awsExports)
 
 ReactDOM.render(
     <React.StrictMode>
