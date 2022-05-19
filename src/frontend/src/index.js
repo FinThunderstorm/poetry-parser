3db27c0d92151ef74bbc91f9583d239fa0632067
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-05-19 16:52:15 +0300

    Initialize frontend

---
+++ src/frontend/src/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Amplify } from 'aws-amplify'
+import App from './App'
+
+import awsExports from './aws-exports'
+
+Amplify.configure(awsExports)
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
