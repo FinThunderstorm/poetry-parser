3965f884ab43b16fa59a0d04eb1370f5fffe3761
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-05-23 13:41:32 +0300

    update JSON structure to include everything needed and tests

--- src/poetry-parser/poetry-parser/tests/parser.test.js
+++ src/poetry-parser/poetry-parser/tests/parser.test.js
@@ -1,13 +1,52 @@
-const { parsePackage, parseFile } = require('../utils/parser')
+const {
+    parsePackage,
+    parseFile,
+    parseDependency,
+    parseRawPackages,
+} = require('../utils/parser')
 
 const atomicwritesResult = require('./mocks/atomicwrites.json')
 const attrsResult = require('./mocks/attrs.json')
+const cachecontrolResult = require('./mocks/cachecontrol.json')
 const idnaResult = require('./mocks/idna.json')
+const importlibmetadataResult = require('./mocks/importlib-metadata.json')
 const pexpectResult = require('./mocks/pexpect.json')
 const platformDirsResult = require('./mocks/platformdirs.json')
 const pluggyResult = require('./mocks/pluggy.json')
 const poetryPluginExportResult = require('./mocks/poetry-plugin-export.json')
 const shellinghamResult = require('./mocks/shellingham.json')
+
+const poetryInput = require('./mocks/poetryLockFileInput.json')
+const poetryOutput = require('./mocks/poetryLockFileOutput.json')
+const poetryRawOutput = require('./mocks/poetryLockFileOutputRaw.json')
+
+describe('parseDependency works correctly', () => {
+    test('basic dependency with just version info works', () => {
+        const result = parseDependency('msgpack = ">=0.5.2"')
+        expect(result).toStrictEqual({
+            name: 'msgpack',
+            optional: false,
+        })
+    })
+    test('basic dependency with flag optional works', () => {
+        const result = parseDependency(
+            'lockfile = {version = ">=0.9", optional = true, markers = "extra == "filecache""}'
+        )
+        expect(result).toStrictEqual({
+            name: 'lockfile',
+            optional: true,
+        })
+    })
+    test('basic dependency with flags but no optional works', () => {
+        const result = parseDependency(
+            'typing-extensions = {version = ">=3.6.4", markers = "python_version < "3.8""}'
+        )
+        expect(result).toStrictEqual({
+            name: 'typing-extensions',
+            optional: false,
+        })
+    })
+})
 
 describe('parsePackage works correctly', () => {
     test('parsePackage works with no extras', () => {
@@ -63,17 +102,32 @@ describe('parsePackage works correctly', () => {
         const result = parsePackage(content)
         expect(result).toStrictEqual(shellinghamResult)
     })
+
+    test('parsePackage works with optionals in two places', () => {
+        const content =
+            '\nname = "cachecontrol"\nversion = "0.12.11"\ndescription = "httplib2 caching for requests"\ncategory = "main"\noptional = false\npython-versions = ">=3.6"\n\n[package.dependencies]\nlockfile = {version = ">=0.9", optional = true, markers = "extra == "filecache""}\nmsgpack = ">=0.5.2"\nrequests = "*"\n\n[package.extras]\nfilecache = ["lockfile (>=0.9)"]\nredis = ["redis (>=2.10.5)"]'
+        const result = parsePackage(content)
+        expect(result).toStrictEqual(cachecontrolResult)
+    })
+
+    test('parsePackage works when no optional, but other attributes given in dependency flags', () => {
+        const content =
+            '\nname = "importlib-metadata"\nversion = "4.11.3"\ndescription = "Read metadata from Python packages"\ncategory = "main"\noptional = false\npython-versions = ">=3.7"\n\n[package.dependencies]\ntyping-extensions = {version = ">=3.6.4", markers = "python_version < "3.8""}\nzipp = ">=0.5"\n\n[package.extras]\ndocs = ["sphinx", "jaraco.packaging (>=9)", "rst.linker (>=1.9)"]\nperf = ["ipython"]\ntesting = ["pytest (>=6)", "pytest-checkdocs (>=2.4)", "pytest-flake8", "pytest-cov", "pytest-enabler (>=1.0.1)", "packaging", "pyfakefs", "flufl.flake8", "pytest-perf (>=0.9.2)", "pytest-black (>=0.3.7)", "pytest-mypy (>=0.9.1)", "importlib-resources (>=1.3)"]\n'
+        const result = parsePackage(content)
+        expect(result).toStrictEqual(importlibmetadataResult)
+    })
 })
 
 describe('parseFile works correctly', () => {
-    test('parseFile works with correctly', () => {
-        const content =
-            '[[package]]\nname = "atomicwrites"\nversion = "1.4.0"\ndescription = "Atomic file writes."\ncategory = "dev"\noptional = false\npython-versions = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*"\n\n[[package]]\nname = "attrs"\nversion = "21.4.0"\ndescription = "Classes Without Boilerplate"\ncategory = "dev"\noptional = false\npython-versions = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*"\n\n[package.extras]\ndev = ["coverage[toml] (>=5.0.2)", "hypothesis", "pympler", "pytest (>=4.3.0)", "six", "mypy", "pytest-mypy-plugins", "zope.interface", "furo", "sphinx", "sphinx-notfound-page", "pre-commit", "cloudpickle"]\ndocs = ["furo", "sphinx", "zope.interface", "sphinx-notfound-page"]\ntests = ["coverage[toml] (>=5.0.2)", "hypothesis", "pympler", "pytest (>=4.3.0)", "six", "mypy", "pytest-mypy-plugins", "zope.interface", "cloudpickle"]\ntests_no_zope = ["coverage[toml] (>=5.0.2)", "hypothesis", "pympler", "pytest (>=4.3.0)", "six", "mypy", "pytest-mypy-plugins", "cloudpickle"]\n\n[[package]]\nname = "pexpect"\nversion = "4.8.0"\ndescription = "Pexpect allows easy control of interactive console applications."\ncategory = "main"\noptional = false\npython-versions = "*"\n\n[package.dependencies]\nptyprocess = ">=0.5"\n\n[metadata]\nlock-version = "1.1"\npython-versions = "^3.7"\ncontent-hash = "2faf18664a94b0dc94c937c24fe8fdbf95aefe3e6bc85e2346fe935047bca353"\n\n[metadata.files]\natomicwrites = [\n    {file = "atomicwrites-1.4.0-py2.py3-none-any.whl", hash = "sha256:6d1784dea7c0c8d4a5172b6c620f40b6e4cbfdf96d783691f2e1302a7b88e197"},\n    {file = "atomicwrites-1.4.0.tar.gz", hash = "sha256:ae70396ad1a434f9c7046fd2dd196fc04b12f9e91ffb859164193be8b6168a7a"},\n]\n'
-        const result = parseFile(content)
-        expect(result).toStrictEqual([
-            atomicwritesResult,
-            attrsResult,
-            pexpectResult,
-        ])
+    test('parseFile works correctly', () => {
+        const result = parseFile(poetryInput.lockFile)
+        expect(result).toStrictEqual(poetryOutput)
+    })
+})
+
+describe('parseRawPackages works correctly', () => {
+    test('parseRawPackages works correctly', () => {
+        const result = parseRawPackages(poetryInput.lockFileRaw)
+        expect(result).toStrictEqual(poetryRawOutput)
     })
 })
